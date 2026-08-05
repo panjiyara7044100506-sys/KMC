9675a0215c153ed23dddae939ccca24fa23c5e4e
Milan Kumar <panjiyara7044100506@gmail.com>
2026-08-05 23:47:49 +0530

    Modified ReportIssue Page

--- Frontend/src/pages/ReportIssue/reportIssue.jsx
+++ Frontend/src/pages/ReportIssue/reportIssue.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import './reportIssue.css'
+import Sidebar from "../../components/Sidebar/sidebar";
 
 function ReportIssue({ show, setShow }) {
     return (
         <div className="report-issue">
+            <Sidebar show={show} setShow={setShow}/>
             <div className={!show ? "hamburger" : "active"}>
                 <img src="\src\assets\hamburger.png" alt="" onClick={() => setShow(true)} />
             </div>
@@ -13,7 +15,8 @@ function ReportIssue({ show, setShow }) {
                 <div className="report-issue-form">
                     <div className="report-issue-info">
                         <h3 className="report-issue-form-h3">problem details</h3>
-                        <label htmlFor="">
+                        <div className="report-issue-info-container">
+                            <label htmlFor="">
                             title
                             <input type="text" placeholder="title" className="report-issue-form-inputs"/>
                         </label>
@@ -30,6 +33,11 @@ function ReportIssue({ show, setShow }) {
                                 <option value="drainage">drainage</option>
                             </select>
                         </label>
+                        <label htmlFor="userfile" style={{alignItems:"flex-start"}}>
+                            Upload Image
+                            <input type="file" className="report-issue-form-inputs"/>
+                        </label>
+                        </div>
                     </div>
                     <div className="report-issue-location-container">
                         <h3 className="report-issue-form-h3">site location</h3>
@@ -39,7 +47,12 @@ function ReportIssue({ show, setShow }) {
                             <label htmlFor="">ctiy<input type="text" placeholder="city" className="report-issue-form-inputs" /></label>
                             <label htmlFor="">state<input type="text" placeholder="state" className="report-issue-form-inputs" /></label>
                             <label htmlFor="">pinode<input type="text" placeholder="pincode" className="report-issue-form-inputs" /></label>
-                            <label htmlFor="">location<input type="text" placeholder="location" className="report-issue-form-inputs" /></label>
+                            <label htmlFor="" style={{alignItems:"flex-start"}}>location
+                               <section style={{display:"flex",flexDirection:"row",gap:"12px"}}>
+                                    <input type="text" placeholder="latitude" className="report-issue-form-inputs" />
+                                    <input type="text" placeholder="longitude" className="report-issue-form-inputs" />
+                               </section>
+                            </label>
                         </div>
                     </div>
                     <div className="report-issue-userinfo-container">
@@ -50,7 +63,7 @@ function ReportIssue({ show, setShow }) {
                             <label htmlFor="">phone number<input type="text" placeholder="phone number" className="report-issue-form-inputs" /></label>
                         </div>
                     </div>
-                    <button style={{alignSelf:"center",marginTop:"20px",padding:"15px 50px",fontSize:"16px",fontWeight:"550",textTransform:"capitalize",border:"none",outline:"none",color:"white",backgroundColor:"#ff5f1f",borderRadius:"10px"}}>Submit</button>
+                    <button style={{alignSelf:"center",marginTop:"20px",padding:"15px 50px",fontSize:"16px",fontWeight:"550",textTransform:"capitalize",border:"none",outline:"none",color:"white",backgroundColor:"#ff5f1f",borderRadius:"10px",boxShadow:" rgba(0, 0, 0, 0.16) 0px 1px 4px",}}>Submit</button>
                 </div>
             </div>
         </div>
